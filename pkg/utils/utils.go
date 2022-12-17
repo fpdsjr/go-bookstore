91b74efd16da16148fda28cf77c7958d099e5d43
Flávio Junior <flavio.junior@tvnsports.com.br>
2022-12-16 22:20:40 -0300

    bookstore

---
+++ pkg/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal([]byte(body), x)
+
+	return
+
+}
